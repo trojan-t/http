d00b838b567ea1b5b000c1ada14e5593f7a9c8a9
Muhammadkhon <muhammadkhonofficial@gmail.com>
2020-11-09 22:29:14 +0500

    s

--- cmd/main.go
+++ cmd/main.go
@@ -1,30 +1,33 @@
 package main
 
-import (
-	"net/url"
-	"log"
-	"net"
-	"os"
-
-	"github.com/trojan-t/http/pkg/server"
-)
-
-func main() {
-	host := "0.0.0.0"
-	port := "9999"
-
-	if err := execute(host, port); err != nil {
-		os.Exit((1))
-	}
+func main(){
 }
 
-func execute(host string, port string) (err error) {
-	srv := server.NewServer(net.JoinHostPort(host, port))
-	srv.Register("/payments", func(req *server.Request) {
-		uri, err := url.ParseRequestURI()
-		id := req.QueryParams["id"]
-		log.Print(id)
-	})
+// import (
+// 	"net/url"
+// 	"log"
+// 	"net"
+// 	"os"
+
+// 	"github.com/trojan-t/http/pkg/server"
+// )
+
+// func main() {
+// 	host := "0.0.0.0"
+// 	port := "9999"
+
+// 	if err := execute(host, port); err != nil {
+// 		os.Exit((1))
+// 	}
+// }
+
+// func execute(host string, port string) (err error) {
+// 	srv := server.NewServer(net.JoinHostPort(host, port))
+// 	srv.Register("/payments", func(req *server.Request) {
+// 		uri, err := url.ParseRequestURI()
+// 		id := req.QueryParams["id"]
+// 		log.Print(id)
+// 	})
 	
-	return srv.Start()
-}
+// 	return srv.Start()
+// }
