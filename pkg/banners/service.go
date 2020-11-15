3c93d861e48434c00a62c7ea9e640cbc41a7d5ae
Muhammadkhon <muhammadkhonofficial@gmail.com>
2020-11-15 17:24:29 +0500

    MayBE

--- pkg/banners/service.go
+++ pkg/banners/service.go
@@ -6,36 +6,56 @@ import (
 	"sync"
 )
 
-// Banner is banner struct
+// Service presents struct
+type Service struct {
+	mu    sync.RWMutex
+	items []*Banner
+}
+
+// NewService creates new service
+func NewService() *Service {
+	return &Service{items: make([]*Banner, 0)}
+}
+
+// Banner presents banner :)
 type Banner struct {
 	ID      int64
 	Title   string
 	Content string
 	Button  string
 	Link    string
-	Image   string
 }
 
-// Service is service struct
-type Service struct {
-	mu    sync.RWMutex
-	items []*Banner
-}
-
-// NewService is function
-func NewService() *Service {
-	return &Service{items: make([]*Banner, 0)}
-}
-
-// GetAll is CRUD method
-func (s *Service) GetAll(ctx context.Context) ([]*Banner, error) {
+// All returns all banners
+func (s *Service) All(ctx context.Context) ([]*Banner, error) {
 	s.mu.RLock()
 	defer s.mu.RUnlock()
 	return s.items, nil
 }
 
-// GetByID is CRUD method
-func (s *Service) GetByID(ctx context.Context, id int64) (*Banner, error) {
+// Save save/updates banner
+func (s *Service) Save(ctx context.Context, item *Banner) (*Banner, error) {
+	var i int64 = 0
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	if item.ID == 0 {
+		i++
+		item.ID = i
+		s.items = append(s.items, item)
+		return item, nil
+	}
+
+	for index, banner := range s.items {
+		if banner.ID == item.ID {
+			s.items[index] = item
+			return item, nil
+		}
+	}
+	return nil, errors.New("item not found")
+}
+
+// ByID return banner by ID
+func (s *Service) ByID(ctx context.Context, id int64) (*Banner, error) {
 	s.mu.RLock()
 	defer s.mu.RUnlock()
 	for _, banner := range s.items {
@@ -43,40 +63,18 @@ func (s *Service) GetByID(ctx context.Context, id int64) (*Banner, error) {
 			return banner, nil
 		}
 	}
-	return nil, errors.New("banner by id not found")
+	return nil, errors.New("item not found")
 }
 
-var starID int64 = 0
-
-// Save is CRUD method
-func (s *Service) Save(ctx context.Context, item *Banner) (*Banner, error) {
-	s.mu.RLock()
-	defer s.mu.RUnlock()
-	if item.ID == 0 {
-		starID++
-		item.ID = starID
-		s.items = append(s.items, item)
-		return item, nil
-	}
-	for i, banner := range s.items {
-		if banner.ID == item.ID {
-			s.items[i] = item
-			return item, nil
-		}
-	}
-
-	return nil, errors.New("banner save error")
-}
-
-// RemoveByID is CRUD method
+// RemoveByID removes banner by ID
 func (s *Service) RemoveByID(ctx context.Context, id int64) (*Banner, error) {
 	s.mu.RLock()
 	defer s.mu.RUnlock()
-	for i, banner := range s.items {
+	for index, banner := range s.items {
 		if banner.ID == id {
-			s.items = append(s.items[:i], s.items[i+1:]...)
+			s.items = append(s.items[:index], s.items[index+1:]...)
 			return banner, nil
 		}
 	}
-	return nil, errors.New("banner remove by id not found")
+	return nil, errors.New("item not found")
 }
